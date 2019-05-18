3a1afc21d4455304f71ee5f9a900357ddee6e66b
nicnad11 <47256159+nicnad11@users.noreply.github.com>
2019-05-17 21:54:30 -0300

    Add files via upload

--- ArraySort.c
+++ ArraySort.c
@@ -2,13 +2,8 @@
  * 
  * ArraySort.c
  * 
- * Created by Jean-Philippe Legault
+ * Created by Jean-Philippe Legault, modified by Joshua Burke and Nicolas Nadeau
  * 
- * Your task is to implement in place sorting using the two available functions
- * swapAdjacent, and compareAdjacent.
- * 
- * Some bug might have been introduced... you will have to find out if there are any!
- * if so, you will have to correct it
  * 
  ********************************/
 #include <stdio.h>
@@ -18,9 +13,9 @@
 void printArray(int *array, int size)
 {
     int i = 0;
-    for(i=0; i<size; i++)
+    for (i = 0; i < size; i++)
     {
-        if(i != 0)
+        if (i != 0)
         {
             printf(", ");
         }
@@ -30,9 +25,9 @@ void printArray(int *array, int size)
 }
 
 void swapAdjacent(int *a, int index)
-{    
+{
     int temp = *(a + index);
-    *(a + index) = *(a + index + 1);    
+    *(a + index) = *(a + index + 1);
     *(a + index + 1) = temp;
 }
 
@@ -41,25 +36,20 @@ int compareAdjacent(int *a, int index)
     return *(a + index) - *(a + index + 1);
 }
 
-/**
- * TODO: implement in place sorting on an array
- * by using the two functions swapAdjacent and compareAdjacent
- */
 void inPlaceSort(int *a, int size)
 {
     int currSize = 0;
-    for(currSize = size-1; currSize >= 0; currSize--)
+    for (currSize = size - 1; currSize >= 0; currSize--)
     {
         int i = 0;
-        for(i = 0; i < currSize; i++)
+        for (i = 0; i < currSize; i++)
         {
-            if(compareAdjacent(a, i) > 0)
+            if (compareAdjacent(a, i) > 0)
             {
                 swapAdjacent(a, i);
             }
         }
     }
-
 }
 
 int main(void)
@@ -67,12 +57,12 @@ int main(void)
 
     int array_size = 0;
     printf("Enter the array size (>0) and the numbers to fill the array with: ");
-    if(!scanf("%d", &array_size))
+    if (!scanf("%d", &array_size))
     {
         printf("ERROR. Must enter an integer.\n");
         return EXIT_FAILURE;
     }
-    else if(array_size < 1)
+    else if (array_size < 1)
     {
         printf("ERROR. array size must be at least 1.\n");
         return EXIT_FAILURE;
@@ -82,9 +72,9 @@ int main(void)
 
     int i = 0;
     int num;
-    for(i = 0; i <= array_size-1; i++)
+    for (i = 0; i <= array_size - 1; i++)
     {
-        if(!(scanf("%d", &num) == 1))
+        if (!(scanf("%d", &num) == 1))
         {
             printf("bad input\n");
             return 0;
